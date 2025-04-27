433820aa78a87f790cc78dde6a7c7bd44d2ddc15
karoljanowski <karol.jj@icloud.com>
2025-04-27 23:27:47 +0200

    fix build error

--- client/src/components/Files/FilesList.tsx
+++ client/src/components/Files/FilesList.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState, useRef } from "react";
+import { useState, useRef } from "react";
 import Manager from "./Manager/Manager";
 import FileItem from "./FileItem";
 import { Button } from "../ui/button";
